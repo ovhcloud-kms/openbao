4f277e03112e5c204d715cccbbacc93433ed2c18
Aeddis Desauw <89919264+ldesauw@users.noreply.github.com>
2026-04-03 19:08:55 +0200

    Revert "Dev/jsauvain/ovh sk unseal"

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -114,7 +114,6 @@ const sidebars: SidebarsConfig = {
                         "configuration/seal/gcpckms",
                         "configuration/seal/kmip",
                         "configuration/seal/ocikms",
-                        "configuration/seal/okms",
                         "configuration/seal/pkcs11",
                         "configuration/seal/static",
                         "configuration/seal/transit",
